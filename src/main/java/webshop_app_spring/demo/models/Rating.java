324e85a6ab206d80194dcfb7a5363ea961e65c08
mariaprang <marie.prangishvili@gmail.com>
2020-12-25 19:57:24 +0400

    Added front-end for rating section on a product

---
+++ src/main/java/webshop_app_spring/demo/models/Rating.java
@@ -0,0 +1,16 @@
+package webshop_app_spring.demo.models;
+
+public class Rating {
+
+    private float stars;
+
+    public Rating(){}
+
+    public float getStars() {
+        return stars;
+    }
+
+    public void setStars(float stars) {
+        this.stars = stars;
+    }
+}
